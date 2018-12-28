a5cac68ec230aa2a0a3a10ba0f768a0cd71bfa2b
Unknown <andre-dede96@hotmail.com>
2018-12-28 21:39:33 -0200

    Commit 20181228-2139

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 var express = require("express");
 var app = new express();
 var handlebars = require("express-handlebars")
-
+var bodyParser = require("body-parser")
+    //Configuração da template engine
     app.engine("handlebars", handlebars({defaultLayout: 'main'}))
     app.set("view engine", "handlebars")
+    app.get("/",(req, res) => {
+        res.render("layouts/main",{
+            body:'<h1>Projeto Inicial</h1>'+
+            '<button onclick="onredirect(\'/form\')">Tela de Formulário</button>'
+        });
+    })
+    //Configuiração do body Parser
+    app.use(bodyParser.urlencoded({extended:false})); 
+    app.use(bodyParser.json());     
+     
+    app.post("/dados",(req, res) => {
+        res.json(req.body);
+    })
 
     app.get("/form",(req, res) => {
         res.render("formulario");
